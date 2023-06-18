2171b34ad4f1b50737f36863a9595def7726b55b
EdemEssang <edemoski.eu@gmail.com>
2023-06-18 19:17:35 +0000

    changed token

--- markdown2html.py
+++ markdown2html.py
@@ -7,6 +7,7 @@ import sys
 import os
 import re
 
+
 def convert_markdown_to_html(input_file, output_file):
     """
     Converts a Markdown file to HTML and writes the output to a file.
@@ -25,7 +26,8 @@ def convert_markdown_to_html(input_file, output_file):
             if match:
                 heading_level = len(match.group(1))
                 heading_text = match.group(2)
-                html_lines.append(f"<h{heading_level}>{heading_text}</h{heading_level}>")
+                html_lines.append(
+                        f"<h{heading_level}>{heading_text}</h{heading_level}>")
             else:
                 html_lines.append(line.rstrip())
 
@@ -33,11 +35,14 @@ def convert_markdown_to_html(input_file, output_file):
     with open(output_file, "w", encoding="utf-8") as f:
         f.write("\n".join(html_lines))
 
-if __name__ == "__main__":
-    # Check that the correct number of arguments were provided
-    if len(sys.argv) != 3:
-        print("Usage: ./markdown2html.py <input_file> <output_file>", file=sys.stderr)
-        sys.exit(1)
+        if __name__ == "__main__":
+
+            # Check that the correct number of arguments were provided
+            if len(sys.argv) != 3:
+                print(
+                        "Usage: ./markdown2html.py <input_file> <output_file>",
+                        file=sys.stderr)
+                sys.exit(1)
 
     # Get the input and output file names from the command-line arguments
     input_file = sys.argv[1]
